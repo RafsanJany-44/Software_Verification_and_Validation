360863150b6b11d6dfbf69b0cec2ae65169578fc
Rafsan Jany <59754354+RafsanJany-44@users.noreply.github.com>
2022-02-28 15:37:50 +0600

    test

--- lab_3/CALCULATOR/test/advanced.test.js
+++ lab_3/CALCULATOR/test/advanced.test.js
@@ -2,8 +2,8 @@ const calculator=require("../src/advanced");
 
 describe("Pow",()=>{
     var BVAdata=[
-        [3,2,1],
-        [11,8,4],
+        [3,2,9],
+        [11,8,214358881],
         [4,2,16]
     ]
     describe.each(BVAdata)('BVA: Pow(%i, %i), Expected: %i',(a,b,expected)=>{
@@ -13,10 +13,10 @@ describe("Pow",()=>{
     });
 
     var DTdata=[
-        [0,89,89],
-        [-17,-35,-52],
-        [65,-12,53],
-        [-78,24,-54]
+        [0,89,0],
+        [-17,-35,-8.595828691938621e-44],
+        [65,-12,1.758084399840934e-22],
+        [-78,24,2.5719970407685146e+45]
     ]
     describe.each(DTdata)('DT: Pow(%i, %i), Expected: %i',(a,b,expected) => {
         test(`returns ${calculator.Pow(a,b)}`,()=>{
@@ -29,8 +29,8 @@ describe("Pow",()=>{
 
 describe("Modulo",()=>{
     var BVAdata=[
-        [2,2,4],
-        [2,5,25],
+        [2,2,0],
+        [2,5,2],
         [4,2,0]
     ]
     describe.each(BVAdata)('BVA: Modulo(%i, %i), Expected: %i',(a,b,expected)=>{
@@ -41,7 +41,7 @@ describe("Modulo",()=>{
 
     var DTdata=[
         [0,89,0],
-        [-17,-35,17],
+        [-17,-35,-17],
         [65,-12,5],
         [-78,24,-6]
     ]
--- lab_3/CALCULATOR/test/basic.test.js
+++ lab_3/CALCULATOR/test/basic.test.js
@@ -32,7 +32,7 @@ describe("Add",()=>{
 describe("Sub",()=>{
     var BVAdata=[
         [1,2,-1],
-        [4,5,9],
+        [4,5,-1],
         [4,6,-2]
     ]
     describe.each(BVAdata)('BVA: substract(%i, %i), Expected: %i',(a,b,expected)=>{
@@ -42,10 +42,10 @@ describe("Sub",()=>{
     });
 
     var DTdata=[
-        [0,89,89],
-        [-17,-35,-52],
-        [65,-12,53],
-        [-78,24,-54]
+        [0,89,-89],
+        [-17,-35,18],
+        [65,-12,77],
+        [-78,24,-102]
     ]
     describe.each(DTdata)('DT: substract(%i, %i), Expected: %i',(a,b,expected) => {
         test(`returns ${calculator.substract(a,b)}`,()=>{
@@ -69,10 +69,10 @@ describe("Mul",()=>{
     });
 
     var DTdata=[
-        [0,89,89],
-        [-17,-35,-52],
-        [65,-12,53],
-        [-78,24,-54]
+        [0,89,0],
+        [-17,-35,595],
+        [65,-12,-780],
+        [-78,24,-1872]
     ]
     describe.each(DTdata)('DT: multiply(%i, %i), Expected: %i',(a,b,expected) => {
         test(`returns ${calculator.multiply(a,b)}`,()=>{
@@ -86,9 +86,9 @@ describe("Mul",()=>{
 
 describe("Div",()=>{
     var BVAdata=[
-        [1,2,3],
-        [4,5,9],
-        [4,6,10]
+        [1,2,0.5],
+        [4,5,0.8],
+        [4,6,0.6666666666666666]
     ]
     describe.each(BVAdata)('BVA: divide(%i, %i), Expected: %i',(a,b,expected)=>{
         test(`returns ${calculator.divide(a,b)}`,()=>{
@@ -97,10 +97,10 @@ describe("Div",()=>{
     });
 
     var DTdata=[
-        [0,89,89],
-        [-17,-35,-52],
-        [65,-12,53],
-        [-78,24,-54]
+        [0,89,0],
+        [-17,-35,0.4857142857142857],
+        [65,-12,-5.416666666666667],
+        [-78,24,-3.25]
     ]
     describe.each(DTdata)('DT: divide(%i, %i), Expected: %i',(a,b,expected) => {
         test(`returns ${calculator.divide(a,b)}`,()=>{
